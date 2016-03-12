d5e0ad49845a208a148b5be7b08dc23696b6acb9
Riceball LEE <snowyu.lee@gmail.com>
2016-03-12 15:14:08 +0100

    + comments for onStop to play2.4

--- app/Startup.java
+++ app/Startup.java
@@ -16,12 +16,24 @@ import play.Logger;
 @Singleton
 //public class Startup implements StartupInterface {
 public class Startup {
+  /* hook the ApplicationLifecycle lifecycle to onStop*/
   @Inject
   public Startup(Application application, ApplicationEvolutions evolutions) {
-    Logger.info("Startup Inject");
+    //hook the ApplicationEvolutions to startup after evolutions.
+    //Logger.info("Startup Inject");
     // load the demo data in dev mode
     if (application.isDev() && (User.find.all().size() == 0)) {
         DemoData.loadDemoData();
     }
+    // lifecycle.addStopHook(() -> {
+    //   Logger.info("Stopping");
+    //   this.shutdown();
+
+    //   Logger.info("Saving modules data");
+    //   for(Module m: controllers.Application.modules){
+    //     m.saveData();
+    //   }
+    //   return F.Promise.pure(null);
+    // });
   }
 }
